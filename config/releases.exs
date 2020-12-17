f927136f9457931f458a8b5156ec2ed9e19d91b8
Grant McLendon <grant@7mind.de>
2020-12-17 14:56:14 -0600

    add static cachemanifest to release config

--- config/releases.exs
+++ config/releases.exs
@@ -4,7 +4,8 @@ config :septa_gong, SeptaGongWeb.Endpoint,
   server: true,
   # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 database_url =
   System.get_env("DATABASE_URL") ||
