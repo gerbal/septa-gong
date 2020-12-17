ba87135b5331dd69d530c76c0dadbeebe0c05d6a
Grant McLendon <grant@7mind.de>
2020-12-17 17:19:12 -0600

    Clear gong animation when multiple gongs in quick succession

--- lib/septa_gong_web/live/gong_live.ex
+++ lib/septa_gong_web/live/gong_live.ex
@@ -6,7 +6,9 @@ defmodule SeptaGongWeb.GongLive do
   @impl true
   def mount(_params, _session, socket) do
     Phoenix.PubSub.subscribe(SeptaGong.PubSub, "users")
-    {:ok, assign(socket, sounds: [], button_class: "", ringing: false), temporary_assigns: [button_class: ""]}
+
+    {:ok, assign(socket, sounds: [], anim_class: "", ringing: false, clear_after: 0),
+     temporary_assigns: [anim_class: ""]}
   end
 
   @impl true
@@ -17,19 +19,30 @@ defmodule SeptaGongWeb.GongLive do
   end
 
   @impl true
-  def handle_info("gong", %{assigns: %{sounds: sounds}} = socket) do
-    Process.send_after(self(), :clear_anim, 750)
+  def handle_info("gong", socket) do
+    Process.send_after(self(), :sound_gong, 1)
+
+    {:noreply, assign(socket, anim_class: "", ringing: false)}
+  end
+
+  def handle_info(:sound_gong, %{assigns: %{sounds: sounds}} = socket) do
+    Process.send_after(self(), :clear_anim, 3000)
 
     {:noreply,
      assign(socket,
        sounds: [SoundEffect.new(:gong) | sounds],
-       button_class: "animate__animated animate__fadeOut",
-       ringing: true
+       anim_class: "animate__animated animate__slower animate__fadeOut",
+       ringing: true,
+       clear_after: DateTime.add(DateTime.utc_now(), 3000, :millisecond)
      )}
   end
 
-  def handle_info(:clear_anim, socket) do
-    {:noreply, assign(socket, button_class: "", ringing: false  )}
+  def handle_info(:clear_anim, %{assigns: %{clear_after: clear_after}} = socket) do
+    if DateTime.compare(DateTime.utc_now(), clear_after) == :gt do
+      {:noreply, assign(socket, anim_class: "", ringing: false)}
+    else
+      Process.send_after(self(), :clear_anim, 500)
+      {:noreply, socket}
+    end
   end
-
 end
--- lib/septa_gong_web/router.ex
+++ lib/septa_gong_web/router.ex
@@ -32,12 +32,4 @@ defmodule SeptaGongWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  # if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: SeptaGongWeb.Telemetry, ecto_repos: [SeptaGong.Repo]
-    end
-  # end
 end
