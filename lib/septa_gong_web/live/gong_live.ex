9b11d8a1400b7703d801ccf209c28ff23ddee9a2
Grant McLendon <grant@7mind.de>
2020-12-17 16:42:32 -0600

    What a nice gong

--- lib/septa_gong_web/live/gong_live.ex
+++ lib/septa_gong_web/live/gong_live.ex
@@ -1,13 +1,12 @@
 defmodule SeptaGongWeb.GongLive do
   use SeptaGongWeb, :live_view
   alias SeptaGong.SoundEffect
-  alias Phoenix.Socket.Broadcast
 
   require Logger
   @impl true
   def mount(_params, _session, socket) do
     Phoenix.PubSub.subscribe(SeptaGong.PubSub, "users")
-    {:ok, assign(socket, sounds: [])}
+    {:ok, assign(socket, sounds: [], button_class: "", ringing: false), temporary_assigns: [button_class: ""]}
   end
 
   @impl true
@@ -19,6 +18,18 @@ defmodule SeptaGongWeb.GongLive do
 
   @impl true
   def handle_info("gong", %{assigns: %{sounds: sounds}} = socket) do
-    {:noreply, assign(socket, sounds: [SoundEffect.new(:gong) | sounds])}
+    Process.send_after(self(), :clear_anim, 750)
+
+    {:noreply,
+     assign(socket,
+       sounds: [SoundEffect.new(:gong) | sounds],
+       button_class: "animate__animated animate__fadeOut",
+       ringing: true
+     )}
   end
+
+  def handle_info(:clear_anim, socket) do
+    {:noreply, assign(socket, button_class: "", ringing: false  )}
+  end
+
 end
